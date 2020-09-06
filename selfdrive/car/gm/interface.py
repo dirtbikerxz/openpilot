d9149410316feed8985f10b5aee1d74d7ce4f719
Felger <jasrapp@gmail.com>
2020-09-06 14:17:18 -0500

    Further refinement of INDI tuning values (#17)
    * Add in tuning from hanabi95's fork
    
    * Update Panda ref
    
    * Pull in some increased temperature values from 7.7
    
    Hopefully allows more usage in hot summer weather.
    
    * Re-enable cruise cancel and gas/brake cancel.
    
    Note: Gas cancel will lead to a mismatch, might need to think about a better solution here.
    
    * Revert "Re-enable cruise cancel and gas/brake cancel."
    
    This reverts commit b524a2df57aa27a7cb2d91567e95757b4485b7e1.
    
    * Update tuning pids to use INDI
    
    * Enable cruise buttons and gas/brake cancel
    
    Will require some troubleshooting to make sure it's functioning correctly.  Possibility of state mismatch between panda and openpilot
    
    * Update panda
    
    * Update to account for possibility of missing attribute
    
    * Update tuning parameters based on real-world tuning
    
    Much better than before, still needs some work.  Solid on turns mostly, and some jitter on straightaways.  Sometimes a little oversteer that needs to be resolved.
    
    * Updated tuning, further drive tuning
    
    Better performance, less oscillation on straightaways, but needs more work.
    
    * Try alternate method of determining if pedal exists.
    
    * Assuming self.CP is the CarParams object, should be able to determine pedal
    
    * Further refinement of INDI tuning values
    
    Almost completely rid of oscillation on straightaways, barely noticeable.  May need some slight additional gain for really good cornering, will tune more as I have time.
    
    * Further improvements on lateral tuning

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -72,10 +72,10 @@ class CarInterface(CarInterfaceBase):
       #
       ret.steerActuatorDelay = 0.15
       ret.lateralTuning.init('indi')
-      ret.lateralTuning.indi.innerLoopGain = 4.2
-      ret.lateralTuning.indi.outerLoopGain = 12.7
-      ret.lateralTuning.indi.timeConstant = 5.0
-      ret.lateralTuning.indi.actuatorEffectiveness = 6.5
+      ret.lateralTuning.indi.innerLoopGain = 4.57 # rate error gain
+      ret.lateralTuning.indi.outerLoopGain = 13.1 # error gain
+      ret.lateralTuning.indi.timeConstant = 5.5
+      ret.lateralTuning.indi.actuatorEffectiveness = 6.79
 
       tire_stiffness_factor = 1.0
 

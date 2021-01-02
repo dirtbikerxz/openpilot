2eb81fd47ab8437731fed9ba13028bb815a5471f
Rohit Gondi <dirtbikerxz@gmail.com>
2021-01-02 02:11:21 -0600

    spinner hotfix pt2

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -3,8 +3,8 @@
 export PASSIVE="0"
 
 exec ./launch_chffrplus.sh  &
-exec ./eon-custom-themes/install_theme.py &
-exec ./install_spinner.sh  &
+chmod 755 ./eon-custom-themes/install_theme.py && exec ./eon-custom-themes/install_theme.py &
+chmod 755 ./install_spinner.sh && ./install_spinner.sh  &
 
 wait
 echo all processes complete
